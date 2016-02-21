1031c35c7b45b6bd3b5a1ab5349e14d412c390fe
Manohar <kvmanohar@gmail.com>
2016-02-21 17:22:41 +0000

    Final product

--- app/src/main/java/com/example/android/cookies/MainActivity.java
+++ app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.cookies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void changeImage(View view) {
+        TextView textView = (TextView) findViewById(R.id.textDisplayMessage);
+        ImageView imgView = (ImageView) findViewById(R.id.cookieImage);
+        Button btView = (Button) findViewById(R.id.button_eat_cookie);
+
+        if (textView.getText() == "I'm so hungry")
+        {
+            textView.setText("I'm so full");
+            imgView.setImageResource(R.drawable.after_cookie);
+            btView.setText("GET COOKIE");
+        }
+        else
+        {
+            textView.setText("I'm so hungry");
+            imgView.setImageResource(R.drawable.before_cookie);
+            btView.setText("EAT COOKIE");
+        }
     }
 }
